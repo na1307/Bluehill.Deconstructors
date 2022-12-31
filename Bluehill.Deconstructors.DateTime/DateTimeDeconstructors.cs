a26ecb9400ee779d303c010918fdc3916393ec5d
na1307 <na1307@outlook.kr>
2022-12-31 15:11:08 +0900

    Add net7.0

---
+++ Bluehill.Deconstructors.DateTime/DateTimeDeconstructors.cs
@@ -0,0 +1,60 @@
+﻿namespace Bluehill.Deconstructors;
+
+public static class DateTimeDeconstructors {
+    public static void Deconstruct(this DateTime dateTime, out DateOnly date, out TimeOnly time) {
+        date = DateOnly.FromDateTime(dateTime);
+        time = TimeOnly.FromDateTime(dateTime);
+    }
+
+    public static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day) {
+        year = dateTime.Year;
+        month = dateTime.Month;
+        day = dateTime.Day;
+    }
+
+    public static void Deconstruct(this DateOnly date, out int year, out int month, out int day) {
+        year = date.Year;
+        month = date.Month;
+        day = date.Day;
+    }
+
+    public static void Deconstruct(this TimeOnly time, out int hour, out int minute) {
+        hour = time.Hour;
+        minute = time.Minute;
+    }
+
+    public static void Deconstruct(this TimeOnly time, out int hour, out int minute, out int second) {
+        hour = time.Hour;
+        minute = time.Minute;
+        second = time.Second;
+    }
+
+    public static void Deconstruct(this TimeOnly time, out int hour, out int minute, out int second, out int millisecond) {
+        hour = time.Hour;
+        minute = time.Minute;
+        second = time.Second;
+        millisecond = time.Millisecond;
+    }
+
+    public static void Deconstruct(this TimeOnly time, out int hour, out int minute, out int second, out int millisecond, out int microsecond) {
+        hour = time.Hour;
+        minute = time.Minute;
+        second = time.Second;
+        millisecond = time.Millisecond;
+        microsecond = time.Microsecond;
+    }
+
+    public static void Deconstruct(this TimeOnly time, out int hour, out int minute, out int second, out int millisecond, out int microsecond, out int nanosecond) {
+        hour = time.Hour;
+        minute = time.Minute;
+        second = time.Second;
+        millisecond = time.Millisecond;
+        microsecond = time.Microsecond;
+        nanosecond = time.Nanosecond;
+    }
+
+    public static void Deconstruct(this DateTimeOffset dateTimeOffset, out DateOnly date, out TimeOnly time, out TimeSpan offset) {
+        (date, time) = dateTimeOffset.DateTime;
+        offset = dateTimeOffset.Offset;
+    }
+}
