47a0e1ebf83dd4c2363cdb2f6d224c0611135839
na1307 <na1307@outlook.kr>
2023-01-01 12:58:56 +0900

    Add Tests

---
+++ Bluehill.Deconstructors.DateTime.Test/DateTimeDeconstructorsTest.cs
@@ -0,0 +1,50 @@
+﻿namespace Bluehill.Deconstructors.DateTime.Test;
+
+public sealed class DateTimeDeconstructorsTest {
+    static readonly System.DateTime now = System.DateTime.Now;
+
+    [Fact]
+    public void DeconstructTest1() {
+        var (dateOnly, timeOnly) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest2() {
+        var (year, month, day) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest3() {
+        var ((year, month, day), _) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest4() {
+        var (_, (hour, minute)) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest5() {
+        var (_, (hour, minute, second)) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest6() {
+        var (_, (hour, minute, second, millisecond)) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest7() {
+        var (_, (hour, minute, second, millisecond, microsecond)) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest8() {
+        var (_, (hour, minute, second, millisecond, microsecond, nanosecond)) = now;
+    }
+
+    [Fact]
+    public void DeconstructTest9() {
+        var (dateOnly, timeOnly, timeSpan) = DateTimeOffset.Now;
+    }
+}
